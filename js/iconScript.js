44de1d58257514d2848bb4a427dc6ffaf0e4cba4
Maria Fernanda <fefepanda@gmail.com>
2024-09-15 16:57:20 -0400

    added icons in contactsform for accessibility

---
+++ js/iconScript.js
@@ -0,0 +1,51 @@
+// each divider needs to have a different id name for it to work :(
+const fdivider = document.getElementById('fdiv');
+const ldivider = document.getElementById('ldiv');
+const edivider = document.getElementById('ediv');
+const pdivider = document.getElementById('pdiv');
+
+// input id name
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const phoneNumber = document.getElementById('phoneNumber');
+
+firstName.addEventListener("input", (e) => {
+    const firstNameValue = e.currentTarget.value; 
+    if(/[A-Za-z]/.test(firstNameValue) == true) {
+        fdivider.classList.add("valid");
+    }
+    else {
+        fdivider.classList.remove("valid");
+    }
+})
+
+lastName.addEventListener("input", (e) => {
+    const lastNameValue = e.currentTarget.value; 
+    if(/[A-Za-z]/.test(lastNameValue) == true) {
+        ldivider.classList.add("valid");
+    }
+    else {
+        ldivider.classList.remove("valid");
+    }
+})
+
+email.addEventListener("input", (e) => {
+    const emailValue = e.currentTarget.value; 
+    if(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]/.test(emailValue) == true) {
+        edivider.classList.add("valid");
+    }
+    else {
+        edivider.classList.remove("valid");
+    }
+})
+
+phoneNumber.addEventListener("input", (e) => {
+    const phoneNumberValue = e.currentTarget.value; 
+    if(/[0-9]{3} [0-9]{3}-[0-9]{4}/.test(phoneNumberValue) == true) {
+        pdivider.classList.add("valid");
+    }
+    else {
+        pdivider.classList.remove("valid");
+    }
+})
