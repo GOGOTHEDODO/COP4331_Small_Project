78126dc2ac5d214a8d160c5f89495f291a294ded
Sean <seaner2@frontier.com>
2024-09-21 19:27:37 -0400

    testing

--- js/code.js
+++ js/code.js
@@ -38,6 +38,10 @@ document.addEventListener("DOMContentLoaded", () => {
     button.addEventListener('click', contactOps.sort);
   });
 
+  window.contactOps = {
+    sortButtons: sortButtons
+  };
+
   const addContactForm = document.getElementById("addContactForm");
   const notificationBox = document.getElementById("notificationBox");
 
