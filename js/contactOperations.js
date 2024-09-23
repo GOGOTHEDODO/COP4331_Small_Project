6c84cddd5e9331face9e2d8b9bc872e76b7e2e51
Sean <seaner2@frontier.com>
2024-09-23 19:12:43 -0400

    test

--- js/contactOperations.js
+++ js/contactOperations.js
@@ -426,10 +426,6 @@ function validateUserInputEdit(row, firstName, lastName, email, phoneNumber) {
     const result = validationResults[key];
     if (!result.valid) {
       hasErrors = true; // Set the flag to true if there's an error
-      const errorMsg = document.createElement("div");
-      errorMsg.className = "error-msg-edit";
-      errorMsg.innerHTML = `<span class="fa fa-exclamation-triangle"></span> ${result.message}`;
-      // errorContainer.appendChild(errorMsg); // Append error message to the error container
     }
   });
 
