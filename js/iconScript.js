ef8401cb460c976226bc4fa676af87e907eb87d8
Maria Fernanda <fefepanda@gmail.com>
2024-09-25 16:45:37 -0400

    fixed notification box position

--- js/iconScript.js
+++ js/iconScript.js
@@ -10,12 +10,6 @@ const patterns = {
     phoneNumber: /^[0-9]{10,12}$/
 };
 
-// const btns = {
-//     save: "Contact Updated!",
-//     delete: "Contact Deleted!",
-//     addContact: "Contact Added!"
-// }
-
 divs.forEach(div => {
     div.addEventListener("input", (e) => {
         const tgt = e.currentTarget;
@@ -31,18 +25,3 @@ divs.forEach(div => {
         }
     });
 });
-
-// This is just a test to see how it would look
-// TO-DO: link the buttons properly
-// buttons.forEach(button => {
-//     button.addEventListener('click', (e) => {
-//         const buttonID = e.currentTarget.id;
-//         if(btns[buttonID]) {
-//             notification.style.display = "block";
-//             text.innerHTML = btns[buttonID];
-//             setTimeout(() => {
-//               notification.style.display = "none";
-//             }, 3000); // Hide after 3 seconds
-//         }
-//     });
-// });
